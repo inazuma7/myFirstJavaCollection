36934ff432185933f80d5198b7ce51cc7a65f24d
inazuma7 <96759468+inazuma7@users.noreply.github.com>
2022-12-29 16:38:51 +0800

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,29 @@
+class ArrayLength {
+
+     public static void main(String args[]) {
+          String[] names = { "Josh", "Mike", "Larry", "Sam", "Leo" };
+          System.out.println("Length of the Array: " + names.length);
+     }
+
+     /*
+      * looping
+      * public static void main(String args[]) {
+      * String[] names = { "Josh", "Mike", "Larry", "Sam", "Leo" };
+      * for (int i = 0; i < names.length; i++) {
+      * System.out.println(names[i]);
+      * }
+      * }
+      */
+
+     /*
+      * for loop
+      * 
+      * public static void main(String args[]) {
+      * String[] names = { "Josh", "Mike", "Larry", "Sam", "Leo" };
+      * for (String name : names) {
+      * System.out.println(name);
+      * }
+      * }
+      */
+
+}
---
+++ FetchArray.java
@@ -0,0 +1,9 @@
+class FetchArray {
+    public static void main(String args[]) {
+        int[] marks = { 69, 75, 81, 67, 93 };
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Marks in Subject " + (i + 1) + " :" + marks[i]);
+        }
+    }
+
+}
